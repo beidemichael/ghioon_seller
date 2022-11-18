699596ee35433557dbf20563073462b3773d0b87
Beidemichael <Beidemichael@gmail.com>
2022-11-18 11:11:18 +0300

    Started Store UI

--- lib/Screens/HomeScreenWidets/store.dart
+++ lib/Screens/HomeScreenWidets/store.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../Shared/customColors.dart';
+
 class Store extends StatefulWidget {
   const Store({super.key});
 
@@ -12,26 +14,18 @@ class _StoreState extends State<Store> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       drawer: Container(),
+      drawer: Container(),
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(70.0),
+        preferredSize: const Size.fromHeight(70.0),
         child: AppBar(
             centerTitle: true,
             title: Row(
               mainAxisSize: MainAxisSize.min,
               // ignore: prefer_const_literals_to_create_immutables
               children: [
-                Text('Store',
+                const Text('Store',
                     style: TextStyle(
                         fontSize: 30.0,
-                        // ignore: prefer_const_literals_to_create_immutables
-                        // shadows: <Shadow>[
-                        //   const Shadow(
-                        //     offset: Offset(0.0, 4.0),
-                        //     blurRadius: 3.0,
-                        //     color: Color.fromARGB(255, 190, 190, 190),
-                        //   ),
-                        // ],
                         color: Colors.white,
                         fontWeight: FontWeight.w700)),
               ],
@@ -39,13 +33,99 @@ class _StoreState extends State<Store> {
             // excludeHeaderSemantics: true,
             backgroundColor: CustomColors().blue,
             // automaticallyImplyLeading: false,
-            elevation: 0,
-            shape: RoundedRectangleBorder(
+            elevation: 5,
+            shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.vertical(
                 bottom: Radius.circular(30),
               ),
             ),
-            iconTheme: IconThemeData(color: Colors.white)),
+            iconTheme: const IconThemeData(color: Colors.white)),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              // height: ,
+
+              decoration: BoxDecoration(
+                // border: Border.all(width: 1, color: Colors.black),
+                color: CustomColors().blue,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12.0, horizontal: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Text('My Store',
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700)),
+                    const Text('Endale Abegazee',
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400)),
+                    const Text('Id: G1547856',
+                        style: TextStyle(
+                            fontSize: 17.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w200)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              // height: ,
+
+              decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Color.fromARGB(255, 224, 224, 224)),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        Icon(FontAwesomeIcons.solidUser,color: CustomColors().blue,size: 20,),
+                        SizedBox(width: 18,),
+                        const Text('Id: G1547856',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500)),
+                      ],
+                    ),
+                    Icon(
+                      FontAwesomeIcons.angleRight,
+                      color: CustomColors().blue,
+                      size: 20,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
