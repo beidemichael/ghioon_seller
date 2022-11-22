2307a969cddf2225cb6b58c5f4a44daf986dd533
Beidemichael <Beidemichael@gmail.com>
2022-11-22 15:15:41 +0300

    Registered for Android, iOS, Web, MacOS on Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ghioon_seller/firebase_options.dart';
 
 import 'Screens/home.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Firebase.apps;
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
