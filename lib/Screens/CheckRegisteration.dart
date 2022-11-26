20b1f1bb8a397b1c9e5ef4c089bc45c7908438a2
Abukiii <abubekerabdu85@gmail.com>
2022-11-26 11:52:56 +0300

    Add ghioon Id feature on seller signup

--- lib/Screens/CheckRegisteration.dart
+++ lib/Screens/CheckRegisteration.dart
@@ -2,11 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:ghioon_seller/Models/models.dart';
 import 'package:ghioon_seller/Screens/SignIn/BecomeSeller.dart';
 import 'package:ghioon_seller/Service/categoryDatabase.dart';
+import 'package:ghioon_seller/Service/lastId.dart';
 import 'package:provider/provider.dart';
 
-import '../Models/models.dart';
 import 'SignIn/PendingScreen.dart';
 import 'home.dart';
 
@@ -26,13 +27,15 @@ class CheckRegisteration extends StatelessWidget {
               size: 50.0,
             ))
           : userInfo.isEmpty
-              ?  
-              StreamProvider<List<Categories>>.value(
-                value: CategoryDatabaseService().categories,
-                initialData: const [],
-                catchError: (_, __) => [],
-                child:  BecomeSeller())
-              
+              ? StreamProvider<List<Categories>>.value(
+                  value: CategoryDatabaseService().categories,
+                  initialData: const [],
+                  catchError: (_, __) => [],
+                  child: StreamProvider<List<LastId>>.value(
+                      value: LastIdService().lastId,
+                      initialData: const [],
+                      catchError: (_, __) => [],
+                      child: BecomeSeller()))
               : userInfo[0].approved
                   ? const HomeScreen()
                   : const PendingScreen(),
