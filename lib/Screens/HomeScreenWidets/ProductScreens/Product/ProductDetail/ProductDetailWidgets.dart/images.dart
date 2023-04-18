9ffcb041617ef5681d555f1bc0704651a711e40a
beidemichael <beidemichael@gmail.com>
2023-04-18 15:44:45 +0300

    Modified product image horizontal swipe: to change the image only once

--- lib/Screens/HomeScreenWidets/ProductScreens/Product/ProductDetail/ProductDetailWidgets.dart/images.dart
+++ lib/Screens/HomeScreenWidets/ProductScreens/Product/ProductDetail/ProductDetailWidgets.dart/images.dart
@@ -24,7 +24,8 @@ class _ImagesListState extends State<ImagesList> {
   bool play = false;
   bool clear = true;
   double currentOffset = 0;
-double threshold = 100;
+  double threshold = 50;
+  bool hasAddedImageIndex = false;
 
   @override
   void initState() {
@@ -53,30 +54,36 @@ double threshold = 100;
     return Stack(
       children: [
         GestureDetector(
-          
           onTap: () {
             print("pressed");
             setState(() {
               clear = !clear;
             });
           },
-
- onHorizontalDragUpdate: (details) {
-    currentOffset += details.delta.dx;
-    if (currentOffset.abs() > threshold) {
-      setState(() {
-        if (details.delta.dx > 0 && ImageIndex > 0) {
-          ImageIndex--;
-        } else if (details.delta.dx < 0 && ImageIndex <widget.product.image.length - 1) {
-          ImageIndex++;
-        }
-        currentOffset = 0;
-      });
-    }
-  },
-          
-       
-          
+          onHorizontalDragUpdate: (details) {
+            currentOffset += details.delta.dx;
+            if (currentOffset.abs() > threshold) {
+              if (!hasAddedImageIndex) {
+                setState(() {
+                  if (details.delta.dx > 0 && ImageIndex > 0) {
+                    ImageIndex--;
+                  } else if (details.delta.dx < 0 &&
+                      ImageIndex < widget.product.image.length - 1) {
+                    ImageIndex++;
+                  }
+                  currentOffset = 0;
+                  hasAddedImageIndex = true;
+                });
+              }
+            }
+          },
+          onHorizontalDragEnd: (details) {
+            if (hasAddedImageIndex) {
+              setState(() {
+                hasAddedImageIndex = false;
+              });
+            }
+          },
           child: ClipRRect(
             borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(8.0),
